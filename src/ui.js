1abd91f4eace5e2036d5d35ab27ef9f7b637c3fe
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-12-29 16:14:35 +0000

    +Add weatherLoad function responsible for populating HTML elements with weather data

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,4 @@
+import { format} from 'date-fns';
 import videoSource from './images/ocean.mp4';
 import weather from './weather';
 
@@ -32,8 +33,34 @@ const loadPageUI = (() => {
         pageBody.appendChild(videoContainer);
     }
 
-    function weatherLoad(data){
+    function weatherLoad(object){
+        // Head section
+        const dataCity = document.querySelector('.data-city');
+        const dataCountry = document.querySelector('.data-country');
+        const dataTime = document.querySelector('.data-time');
 
+        dataCity.textContent = `${object.name}`;
+        dataCountry.textContent = `${object.country}`;
+        dataTime.textContent =  format(object.localtime, 'EEEE dd MMMM yyyy | H:mm');
+        // Weather section - TOP
+        const dataIcon = document.querySelector('.data-icon');
+        const dataTemp = document.querySelector('.data-temp');
+
+        dataIcon.src = `https:${object.condition[1]}`;
+        dataIcon.alt = 'Weather Icon';
+        dataTemp.textContent = `${Math.round(object.temp.temp_c)}°C`;
+        // Weather section - BOT
+        const dataFeel = document.querySelector('.data-feel');
+
+        dataFeel.textContent = `Feels like ${Math.round(object.feelslike.feelslike_c)}°C`;
+        // Details section
+        const dataHumidity = document.querySelector('.data-humidity');
+        const dataGust = document.querySelector('.data-gust');
+        const dataWind = document.querySelector('.data-wind');
+
+        dataHumidity.textContent = `${object.humidity}`;
+        dataGust.textContent = `${object.gust.gust_kph}`;
+        dataWind.textContent = `${object.wind.wind_kph}`;
     }
 
     function search(){
@@ -46,6 +73,7 @@ const loadPageUI = (() => {
     return {
         initialLoad,
         search,
+        weatherLoad,
     }
 })()
 
