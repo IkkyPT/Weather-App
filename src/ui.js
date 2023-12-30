4681cc244843590e548d1f419487a776a2c1842b
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-12-30 23:12:14 +0000

    Enhance UI and user interaction
    1.Unit Conversion Feature:
       - Added metric and imperial unit conversion buttons for temperature and speed.
       - Implemented dynamic updating of UI elements based on the selected units.
    
    2.Code Organization:
       - Reorganized code for better readability and maintainability.
       - Introduced a variable (`currentObject`) to store and manage the current weather data.
    
    3.Bug Fix:
       - Resolved an issue where the error message span was not hidden after a successful search.
    
    4.Comments:
       - Added comments to key sections of the code for better understanding and documentation.

--- src/ui.js
+++ src/ui.js
@@ -3,22 +3,41 @@ import videoSource from './images/ocean.mp4';
 import weather from './weather';
 
 const loadPageUI = (() => {
+
+    let currentObject; // Variable to store the current weather data
+
     function initialLoad(){
+        // Load the background video and set up event listeners
         loadBg();
         loadEvent();
     }
 
     function loadEvent() {
+        // Set up event listeners for metric, imperial, and search form
         const searchForm = document.getElementById('searchForm');
+        const metric = document.querySelector('.settings-metric');
+        const imperial = document.querySelector('.settings-imperial');
+
+        metric.addEventListener('click', () => {
+            const celsius = "°C";
+            const kph = "kph";
+            clickUnits(celsius, kph, currentObject);
+        });
+
+        imperial.addEventListener('click', () => {
+            const fahrenheit = "°F";
+            const mph = "mph";
+            clickUnits(fahrenheit, mph, currentObject);
+        });
 
         searchForm.addEventListener('submit', (event) => {
             event.preventDefault();
             search();
         })
-
     }
 
     function loadBg(){
+        // Load background video
         const pageBody = document.querySelector('.main-content');
         const videoContainer = document.createElement('div');
         videoContainer.id = 'video-container';
@@ -34,6 +53,7 @@ const loadPageUI = (() => {
     }
 
     function weatherLoad(object){
+        // Populate UI elements with weather data
         // Head section
         const dataCity = document.querySelector('.data-city');
         const dataCountry = document.querySelector('.data-country');
@@ -58,22 +78,62 @@ const loadPageUI = (() => {
         const dataGust = document.querySelector('.data-gust');
         const dataWind = document.querySelector('.data-wind');
 
-        dataHumidity.textContent = `${object.humidity}`;
-        dataGust.textContent = `${object.gust.gust_kph}`;
-        dataWind.textContent = `${object.wind.wind_kph}`;
+        dataHumidity.textContent = `${object.humidity}%`;
+        dataGust.textContent = `${object.gust.gust_kph} kph`;
+        dataWind.textContent = `${object.wind.wind_kph} kph`;
+
+        currentObject = object; // Update the current weather data
     }
 
-    function search(){
+    function clickUnits(temperatureUnit, speedUnit, object) {
+        // Update UI elements based on selected units (metric or imperial)
+        const dataTemp = document.querySelector('.data-temp');
+        const dataFeel = document.querySelector('.data-feel');
+        const dataWind = document.querySelector('.data-wind');
+        const dataGust = document.querySelector('.data-gust');
+
+        // Update temperature and feels-like values
+        const temperatureValue = temperatureUnit === "°C" ? object.temp.temp_c : object.temp.temp_f;
+        const feelslikeValue = temperatureUnit === "°C" ? object.feelslike.feelslike_c : object.feelslike.feelslike_f;
+        dataTemp.textContent = `${Math.round(temperatureValue)}${temperatureUnit}`;
+        dataFeel.textContent = `Feels like ${Math.round(feelslikeValue)}${temperatureUnit}`;
+
+        const windValue = speedUnit === "kph" ? object.wind.wind_kph : object.wind.wind_mph;
+        const gustValue = speedUnit === "kph" ? object.gust.gust_kph : object.gust.gust_mph;
+        dataWind.textContent = `${windValue}${speedUnit}`;
+        dataGust.textContent = `${gustValue}${speedUnit}`;
+    }
+
+    function search() {
+        // Handle the user search input
         const userSearch = document.getElementById('search');
         const userSearchValue = userSearch.value;
 
-        weather.getWeather(userSearchValue);
+        // Check if the user input is not empty
+        if (userSearchValue.trim() !== '') {
+            const errorMsg = document.querySelector('.errorMsg');
+            errorMsg.style.visibility = "hidden";
+
+            // Fetch weather data
+            weather.getWeather(userSearchValue);
+        } else {
+            loadPageUI.throwError();
+        }
+    }
+
+    function throwError(){
+        // Show error message for invalid location
+        const errorMsg = document.querySelector('.errorMsg'); 
+        
+        errorMsg.style.visibility = "visible";
+        errorMsg.textContent = "No location found!"  
     }
 
     return {
         initialLoad,
         search,
         weatherLoad,
+        throwError,
     }
 })()
 
